a21c77cd91711415cf08db4e37219287f6eb33cf
Kauan Santos Lima <kauansantosl2006@gmail.com>
2024-10-03 14:43:54 +0000

    T002: init - Criação inicial dos componentes FormCadUser e FormLoginUser

---
+++ autenticacao-firebase/src/app/components/form-cad-user/form-cad-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-cad-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-cad-user.component.html',
+  styleUrl: './form-cad-user.component.css'
+})
+export class FormCadUserComponent {
+
+}
---
+++ autenticacao-firebase/src/app/components/form-login-user/form-login-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-login-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-login-user.component.html',
+  styleUrl: './form-login-user.component.css'
+})
+export class FormLoginUserComponent {
+
+}
