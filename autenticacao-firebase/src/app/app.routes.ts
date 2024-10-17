b5e28b862f7f2b07526057e7214bb05f90b7f992
Kauan Santos Lima <kauansantosl2006@gmail.com>
2024-10-17 14:04:25 +0000

    T002: chore -  Definida as rotas do projeto

--- autenticacao-firebase/src/app/app.routes.ts
+++ autenticacao-firebase/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { FormCadUserComponent } from './components/form-cad-user/form-cad-user.component';
+import { FormLoginUserComponent } from './components/form-login-user/form-login-user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path:'login',
+    loadComponent: () => FormLoginUserComponent,
+    title: 'FireAuth - Login'
+  },
+  {
+    path:'register',
+    loadComponent: () => FormCadUserComponent,
+    title: 'FireAuth - Cadastro'
+  }
+];
