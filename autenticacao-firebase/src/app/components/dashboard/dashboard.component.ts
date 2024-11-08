1a79b1504c8b80bdd401fd83de2d3fe4ec4bcde8
Kauan Santos Lima <kauansantosl2006@gmail.com>
2024-11-08 11:51:38 +0000

    T005: feat - Implementação do método sair do componente Dashboard

--- autenticacao-firebase/src/app/components/dashboard/dashboard.component.ts
+++ autenticacao-firebase/src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  readonly usuarioLogado: string = 'testeUsuario@teste.com';
 
+  constructor (
+    private readonly navRouter: Router
+  ) {}
+
+  sair() {
+    this.navRouter.navigateByUrl('/login');
+  }
 }
